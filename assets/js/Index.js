7aac2c4513a5f236a7195c3272f4d8451899e8c8
unknown <=mujtabanaseer.pakistan@gmail.com>
2020-12-28 15:39:49 +0500

    working on index.js

---
+++ assets/js/Index.js
@@ -0,0 +1,25 @@
+let z;
+function plus(a,b){
+    z=a+b;
+    return z;
+}
+function minus(a,b){
+    z=a-b;
+    return z;
+}
+function divide(a,b){
+    z=a/b;
+    return z;
+}
+function multiply(a,b){
+    z=a*b;
+    return z;
+}
+let  w=plus(5,5);
+let x=minus(10,5);
+let y=divide(10,5);
+let zz=multiply(2,2);
+console.log(w);
+console.log(x);
+console.log(y);
+console.log(zz);
